5597dcb461afe6bcd44befb3ca0abaf4329b342b
etherfoun <kjkrbyu2004@gmail.com>
2024-11-11 19:53:50 +0200

    Added test

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM jenkins/jenkins:lts
+
+USER root
+
+RUN curl -fsSL https://deb.nodesource.com/setup_14.x | bash - && \
+    apt-get install -y nodejs && \
+    rm -rf /var/lib/apt/lists/*
+
+USER jenkins
---
+++ app.test.js
@@ -0,0 +1,17 @@
+// app.test.js
+const request = require('supertest');
+const app = require('./app');
+const assert = require('assert');
+
+describe('GET /', function() {
+  it('responds with Hello World!', function(done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        assert.strictEqual(res.text, 'Hello World!');
+        done();
+      });
+  });
+});
